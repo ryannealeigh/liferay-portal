fbf176d610a37d8ff438bc8083b79cdbc567c1c2
William Nealeigh <william.nealeigh@liferay.com>
2019-08-19 13:11:22 -0700

    LPS-89065 regen

--- portal-kernel/src/com/liferay/asset/kernel/service/AssetEntryLocalService.java
+++ portal-kernel/src/com/liferay/asset/kernel/service/AssetEntryLocalService.java
@@ -66,15 +66,6 @@ public interface AssetEntryLocalService
 	 *
 	 * Never modify or reference this interface directly. Always use {@link AssetEntryLocalServiceUtil} to access the asset entry local service. Add custom service methods to <code>com.liferay.portlet.asset.service.impl.AssetEntryLocalServiceImpl</code> and rerun ServiceBuilder to automatically copy the method declarations to this interface.
 	 */
-	public void addAssetCategoryAssetEntries(
-		long categoryId, List<AssetEntry> assetEntries);
-
-	public void addAssetCategoryAssetEntries(long categoryId, long[] entryIds);
-
-	public void addAssetCategoryAssetEntry(
-		long categoryId, AssetEntry assetEntry);
-
-	public void addAssetCategoryAssetEntry(long categoryId, long entryId);
 
 	/**
 	 * Adds the asset entry to the database. Also notifies the appropriate model listeners.
@@ -94,8 +85,6 @@ public interface AssetEntryLocalService
 
 	public void addAssetTagAssetEntry(long tagId, long entryId);
 
-	public void clearAssetCategoryAssetEntries(long categoryId);
-
 	public void clearAssetTagAssetEntries(long tagId);
 
 	/**
@@ -106,17 +95,6 @@ public interface AssetEntryLocalService
 	 */
 	@Transactional(enabled = false)
 	public AssetEntry createAssetEntry(long entryId);
-
-	public void deleteAssetCategoryAssetEntries(
-		long categoryId, List<AssetEntry> assetEntries);
-
-	public void deleteAssetCategoryAssetEntries(
-		long categoryId, long[] entryIds);
-
-	public void deleteAssetCategoryAssetEntry(
-		long categoryId, AssetEntry assetEntry);
-
-	public void deleteAssetCategoryAssetEntry(long categoryId, long entryId);
 
 	/**
 	 * Deletes the asset entry from the database. Also notifies the appropriate model listeners.
@@ -249,30 +227,6 @@ public interface AssetEntryLocalService
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public List<AssetEntry> getAncestorEntries(long entryId)
 		throws PortalException;
-
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
-	public List<AssetEntry> getAssetCategoryAssetEntries(long categoryId);
-
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
-	public List<AssetEntry> getAssetCategoryAssetEntries(
-		long categoryId, int start, int end);
-
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
-	public List<AssetEntry> getAssetCategoryAssetEntries(
-		long categoryId, int start, int end,
-		OrderByComparator<AssetEntry> orderByComparator);
-
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
-	public int getAssetCategoryAssetEntriesCount(long categoryId);
-
-	/**
-	 * Returns the categoryIds of the asset categories associated with the asset entry.
-	 *
-	 * @param entryId the entryId of the asset entry
-	 * @return long[] the categoryIds of asset categories associated with the asset entry
-	 */
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
-	public long[] getAssetCategoryPrimaryKeys(long entryId);
 
 	/**
 	 * Returns a range of all the asset entries.
@@ -428,12 +382,6 @@ public interface AssetEntryLocalService
 		String[] className, boolean asc, int start, int end);
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
-	public boolean hasAssetCategoryAssetEntries(long categoryId);
-
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
-	public boolean hasAssetCategoryAssetEntry(long categoryId, long entryId);
-
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public boolean hasAssetTagAssetEntries(long tagId);
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
@@ -550,8 +498,6 @@ public interface AssetEntryLocalService
 		long classTypeId, String userName, String title, String description,
 		String assetCategoryIds, String assetTagNames, boolean showNonindexable,
 		int[] statuses, boolean andSearch);
-
-	public void setAssetCategoryAssetEntries(long categoryId, long[] entryIds);
 
 	public void setAssetTagAssetEntries(long tagId, long[] entryIds);
 
